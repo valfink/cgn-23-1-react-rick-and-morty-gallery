23cdb3b410370b879d12a12e6a7f344e9fd90292
valfink <valentin.finke@gmail.com>
2023-02-14 17:48:11 +0100

    Make Cards of not Alive Characters grey

--- src/CharacterCard.tsx
+++ src/CharacterCard.tsx
@@ -7,7 +7,7 @@ type CharacterCardProps = {
 
 export default function CharacterCard(props: CharacterCardProps) {
     return (
-        <div className="card">
+        <div className={`card ${props.character.status !== 'Alive' ? 'notAlive' : ''}`}>
             <h3>{props.character.name}</h3>
             <img src={props.character.image} alt="Character" />
             <div>Lives at: {props.character.location.name}</div>
