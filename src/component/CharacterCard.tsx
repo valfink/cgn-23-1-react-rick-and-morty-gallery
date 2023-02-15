3a5aece72ab790e756957fb1fa6deb1f380efbac
valfink <valentin.finke@gmail.com>
2023-02-15 14:43:51 +0100

    Make CSS even more beautiful, incredible!

--- src/component/CharacterCard.tsx
+++ src/component/CharacterCard.tsx
@@ -9,9 +9,9 @@ type CharacterCardProps = {
 export default function CharacterCard(props: CharacterCardProps) {
     return (
         <div className={`card ${props.character.status !== 'Alive' ? 'notAlive' : ''}`}>
-            <h3>{props.character.name}</h3>
+            <h2>{props.character.name}</h2>
             <img src={props.character.image} alt="Character" />
-            <div>Lives at: {props.character.location.name}</div>
+            <p>Lives at: {props.character.location.name}</p>
         </div>
     )
 }
