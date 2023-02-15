5b1ed634070a6196bada728f28a493b8d8fe6395
valfink <valentin.finke@gmail.com>
2023-02-15 12:27:23 +0100

    Change import JSON statements for cleaner code

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import Gallery from "./component/Gallery";
 import {Character} from "./model/Character";
+import jsonCharacters from "./characters.json";
 
 function App() {
-    const characters: Character[] = require('./characters.json');
+    const characters: Character[] = jsonCharacters;
   return (
     <div className="App">
         <h1>The Rick and Morty Characters Gallery</h1>
