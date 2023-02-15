0a590637a4b2e3d98bb107fa2e2ec5cdef9ac72e
valfink <valentin.finke@gmail.com>
2023-02-15 16:06:19 +0100

    Rename props

--- src/component/TwoButtonsAndNumber.tsx
+++ src/component/TwoButtonsAndNumber.tsx
@@ -1,10 +1,10 @@
-type ButtonAndNumberProps = {
+type TwoButtonsAndNumberProps = {
     currentNumber: number,
     increaseCurrentNumber: () => void,
     decreaseCurrentNumber: () => void
 }
 
-export default function TwoButtonsAndNumber(props: ButtonAndNumberProps) {
+export default function TwoButtonsAndNumber(props: TwoButtonsAndNumberProps) {
     return (
         <>
             <button onClick={props.decreaseCurrentNumber}>-</button> {props.currentNumber} <button onClick={props.increaseCurrentNumber}>+</button>
