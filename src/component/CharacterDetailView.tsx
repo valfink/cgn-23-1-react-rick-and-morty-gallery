01c3ae5db355c436b44da94b76bff94c0f138fad
valfink <117605562+valfink@users.noreply.github.com>
2023-02-21 14:44:24 +0100

    Add related Episodes in Character Detail View

--- src/component/CharacterDetailView.tsx
+++ src/component/CharacterDetailView.tsx
@@ -1,6 +1,10 @@
 import {Link, useParams} from "react-router-dom";
 import {Character} from "../model/Character";
 import "./CharacterDetailView.css"
+import {useEffect, useState} from "react";
+import {Episode} from "../model/Episode";
+import axios from "axios";
+import EpisodeGallery from "./EpisodeGallery";
 
 type CharacterDetailViewProps = {
     characters: Character[]
@@ -9,8 +13,24 @@ type CharacterDetailViewProps = {
 export default function CharacterDetailView(props: CharacterDetailViewProps) {
     const params = useParams();
     const characterId: string | undefined = params.characterId;
+    const [relatedEpisodes, setRelatedEpisodes] = useState<Episode[]>([]);
 
     const character: Character | undefined = props.characters.find(c => c.id === parseInt(characterId ? characterId : ""));
+
+    useEffect(() => {
+        const abortController = new AbortController();
+        if (character) {
+            setRelatedEpisodes([]);
+            character.episode.forEach(episodeURL => {
+                axios.get(episodeURL, {signal: abortController.signal})
+                    .then(response => {
+                        setRelatedEpisodes(eps => [...eps, response.data]);
+                    })
+                    .catch(console.error);
+            })
+        }
+        return () => { abortController.abort(); }
+    }, [character])
 
     return (
         <>
@@ -24,10 +44,13 @@ export default function CharacterDetailView(props: CharacterDetailViewProps) {
                         <dt>Created:</dt><dd>{character.created}</dd>
                         <dt>Gender:</dt><dd>{character.gender}</dd>
                         <dt>URL:</dt><dd>{character.url}</dd>
+                        <dt>Episode Count:</dt><dd>{character.episode.length}</dd>
                     </dl>
+                    <h3>Episodes</h3>
+                    <EpisodeGallery episodes={relatedEpisodes} />
                 </div>
             }
-            <br/><Link to={"/"}>Back to List</Link>
+            <br/><Link to={"/characters"}>Back to List</Link>
         </>
     );
 }
